89517854c378e739268bad742f049f2e5e0b0695
Emanuele <emanuelebabolo@outlook.it>
2022-07-07 02:23:45 +0200

    d

--- listener.php
+++ listener.php
@@ -1,23 +1,44 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-
 $env = new Env\Dotenv('.env');
-
-$value = $env->get('REPOSITORY');
 $imageName = $env->get('IMAGE_NAME');
 $imageDir =  $env->get('IMAGE_DIR');
-$imageRunning = exec('docker ps | grep ' . $imageName);
-$imageExisting = exec('docker images | grep ' . $imageName);
-if (!$imageExisting) {
-} else {
-    if (!$imageRunning) {
-    } else {
-        exec('docker stop ' . $imageName . ' -t 0');
-    }
-    exec('docker rmi -f ' . $imageName);
+$updateCoolDown = $env->get('UPDATE_COOLDOWN');
+
+function checkForUpdates(){
+    global $imageDir;
+    return exec('cd '.$imageDir.' && git fetch');
 }
 
-exec('docker rm ' . $imageName);
-exec('cd '.$imageDir.' && git pull');
-exec('docker build --tag ' . $imageName . ' ' . $imageDir);
-exec('docker run --name '.$imageName. ' '.$imageName);
+
+function update()
+{
+    global $imageName, $imageDir;
+    $imageRunning = exec('docker ps | grep ' . $imageName);
+    $imageExisting = exec('docker images | grep ' . $imageName);
+
+    if (!$imageExisting) {
+    } else {
+        if (!$imageRunning) {
+        } else {
+            exec('docker stop ' . $imageName . ' -t 0');
+        }
+        exec('docker rmi -f ' . $imageName);
+    }
+
+    exec('docker rm ' . $imageName);
+    exec('cd ' . $imageDir . ' && git pull');
+    exec('docker build --tag ' . $imageName . ' ' . $imageDir);
+    exec('docker run --name ' . $imageName . ' ' . $imageName);
+}
+
+
+
+function loop()
+{
+    global $updateCoolDown;
+    if (checkForUpdates()) {
+        update();
+    }
+    sleep($updateCoolDown);
+}
